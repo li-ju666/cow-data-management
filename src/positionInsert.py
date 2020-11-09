60319b3c3bf7567f5b3f09ba255ee411b5197625
liju666 <li.ju.0122@student.uu.se>
2020-11-09 23:42:48 +0100

    sql: fc class finished+skeleton

---
+++ src/positionInsert.py
@@ -0,0 +1,129 @@
+import mysql.connector
+from pandas import read_csv
+import numpy
+import time
+from src.lib.typeclass import FA, PA
+
+
+# Read data from CSV file
+def readData(filename):
+    data = read_csv(filename, sep=",")
+    return numpy.array(data)
+
+def checkDatabase(dbName, database):
+    cursor = database.cursor()
+    statement = "CREATE DATABASE IF NOT EXISTS "+dbName
+    cursor.execute(statement)
+    database.commit()
+    cursor.close()
+
+# create new table:
+# Side effect: create new table in database
+#
+def checkPositionTable(tableName, database):
+    cursor = database.cursor()
+    statement = "CREATE TABLE IF NOT EXISTS "+tableName + \
+                " (type VARCHAR(10), " \
+                "time1 TIMESTAMP(3), " \
+                "time2 TIMESTAMP(3), " \
+                "posX SMALLINT UNSIGNED, " \
+                "posY SMALLINT UNSIGNED, " \
+                "posZ SMALLINT UNSIGNED, " \
+                "intv INT UNSIGNED, " \
+                "act SMALLINT UNSIGNED, " \
+                "dist SMALLINT UNSIGNED, " \
+                "per SMALLINT UNSIGNED, " \
+                "dur INT UNSIGNED, " \
+                "PRIMARY KEY (time1, type))"
+    print(statement)
+    cursor.execute(statement)
+    database.commit()
+    cursor.close()
+
+def checkAndInsertTable(tableNames, database):
+    cursor = database.cursor()
+    cursor.execute("SHOW TABLES")
+    existingTables = cursor.fetchall()
+    existingTables = ["`"+table[0][0]+"`" for table in existingTables]
+    toBeCreated = list(set(tableNames).difference(set(existingTables)))
+    for tableName in toBeCreated:
+        checkPositionTable(tableName, database)
+
+def connect():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="example"
+    )
+
+    ## make sure database position exists: if not, create it
+    checkDatabase("position", db)
+
+    ## connect to position database
+    positiondb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="example",
+        database="position"
+    )
+    return positiondb
+
+def getTableNames(data):
+    tableNames = numpy.unique(data[:, 2])
+    return list(map(lambda x: "`" + x + "`", tableNames))
+
+def insertFA(fileName):
+    start = time.time()
+    ## data read
+    path = "data/position/"
+    data = readData(path+fileName)
+    tableNames = getTableNames(data)
+
+    ## connect to sql server
+    positiondb = connect()
+
+    ## table check
+    checkAndInsertTable(tableNames, positiondb)
+
+    ## data preparation
+    fa = FA()
+    vals = fa.convert(data, tableNames)
+
+    ## data insertion
+    insertStart = time.time()
+    fa.insert(positiondb, vals)
+    positiondb.close()
+
+    ## time test
+    print("Total time: ", time.time()-start)
+    print("Insertion time: ", time.time()-insertStart)
+
+def insertPA(fileName):
+    start = time.time()
+    ## data read
+    path = "data/position/"
+    data = readData(path+fileName)
+    tableNames = getTableNames(data)
+
+    ## connect to sql server
+    positiondb = connect()
+
+    ## table check
+    checkAndInsertTable(tableNames, positiondb)
+
+    ## data preparation
+    pa = PA()
+    vals = pa.convert(data, tableNames)
+    return vals
+    # ## data insertion
+    # insertStart = time.time()
+    # pa.insert(positiondb, vals)
+    # positiondb.close()
+    #
+    # ## time test
+    # print("Total time: ", time.time()-start)
+    # print("Insertion time: ", time.time()-insertStart)
+
+
+#insertFA("FA_20200921T000000UTC.csv")
+vals = insertPA("PA_20200921T000000UTC.csv")
