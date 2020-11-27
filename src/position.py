b8dbe57c3f140fabbcef10337638270a3a5db9e8
liju666 <li.ju.0122@student.uu.se>
2020-11-27 11:10:50 +0100

    update_cowinfo preprocessing

--- src/position.py
+++ src/position.py
@@ -1,30 +1,18 @@
-import time
-from pandas import read_csv
-import numpy
 from src.lib.dbinit import connect
+from src.lib.read import readPos
 
-def readData(filename):
-    data = read_csv(filename, sep=",")
-    return numpy.array(data)
 
 def insertpos(fileName, Insertor):
-    start = time.time()
     ## data read
-    data = readData(fileName)
+    data = readPos(fileName)
 
     ## connect to sql server
     db = connect()
     #
     ## data preparation
     insertor = Insertor()
-    # tableName = insertor.type
     vals = insertor.convert(data)
-    # return vals
+
     ## data insertion
-    insertStart = time.time()
     insertor.insert(db, vals)
     db.close()
-
-    # time test
-    print("Total time: ", time.time()-start)
-    print("Insertion time: ", time.time()-insertStart)
