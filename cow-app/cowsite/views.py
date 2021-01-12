36102ebd80fd1b930f9f4ff863d6de8d6ed7e42c
Li Ju <li.ju.0122@student.uu.se>
2021-01-12 13:56:03 +0100

    front-end: instructions + robust

--- cow-app/cowsite/views.py
+++ cow-app/cowsite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from src.backend.apis.bgAPIs import bgInfoScan, bgPosScan, bgPosQuery, bgInfoQuery
 from src.backend.apis.overview import overview_func, size_overview
 from functions import format_overview, milkdata_context, position_context, cowinfo_context
+from src.backend.apis.query import positionQuery, infoQuery
 
 
 def index(request):
@@ -19,20 +20,38 @@ def upload(request):
 
 
 def overview(request):
-   over = overview_func()
-   list_info, list_pos = format_overview(over)
+   context = {}
+   try:
+      over = overview_func()
+      list_info, list_pos = format_overview(over)
 
-   # list_size = [['CowInfo', 1489, '0.09 MB'], ['HealthInfo', 244, '0.05 MB'], ['FA', 0, '0.02 MB'], ['InsemInfo', 277, '0.02 MB'], ['MilkInfo', 0, '0.02 MB'], ['PA', 0, '0.02 MB'], ['PAA', 0, '0.02 MB'], ['PC', 0, '0.02 MB'], ['Reference', 249, '0.02 MB']]
-   list_size = size_overview()
-   context = {
-      'info_header': ['KO','Health','Avkastn','Milk'],
-      'position_header': ['FA','PA','PAA','PC'],
-      'size_header': ['Table name', 'Nr of Records', 'Size (MB)'],
-      'list_pos': list_pos,
-      'list_info': list_info,
-      'list_size': list_size,
-   }
-   return render(request, "overview.html", context)
+      # list_size = [['CowInfo', 1489, '0.09 MB'], ['HealthInfo', 244, '0.05 MB'], ['FA', 0, '0.02 MB'], ['InsemInfo', 277, '0.02 MB'], ['MilkInfo', 0, '0.02 MB'], ['PA', 0, '0.02 MB'], ['PAA', 0, '0.02 MB'], ['PC', 0, '0.02 MB'], ['Reference', 249, '0.02 MB']]
+      list_size = size_overview()
+      print("Overview function working fine")
+      context = {
+         'info_header': ['KO','Health','Avkastn','Milk'],
+         'position_header': ['FA','PA','PAA','PC'],
+         'size_header': ['Table name', 'Nr of Records', 'Size (MB)'],
+         'list_pos': list_pos,
+         'list_info': list_info,
+         'list_size': list_size,
+      }
+      # context['status_messsage'] = "Database connected"
+   except Exception as error:
+      print('Error: ')
+      print(error)
+      context = {
+         'info_header': ['KO','Health','Avkastn','Milk'],
+         'position_header': ['FA','PA','PAA','PC'],
+         'size_header': ['Table name', 'Nr of Records', 'Size (MB)'],
+         'list_pos': [],
+         'list_info': [],
+         'list_size': [],
+      }
+      context['status_message'] = 'Failed to connect to database, the following error message were passed: ' + str(error)
+   finally:
+      return render(request, "overview.html", context)
+
 
 
 def dblist(request):  
@@ -42,31 +61,37 @@ def dblist(request):
 def dblist_position(request):
 
    if request.method == 'POST':
-      try:
-         context = position_context(request)
-         print(context)
-         if context['cow_id'] == '':
-            cow_id = []
-         else:
-            cow_id = list(map(int, context['cow_id'].split(',')))
-         if context['group_nr'] == '':
-            grp = []
-         else:
-            grp = list(map(int, context['group_nr'].split(',')))
-         stats = context['status_list']
-         types = context['position_list']
-         start_date = context['start_date']
-         end_date = context['end_date']
-         start_time = context['start_time']
-         end_time = context['end_time']
-         periodic = context['periodic']
-         bgPosQuery(cow_id, grp, stats, types,
-                    start_date, end_date, start_time, end_time, periodic)
-      except Exception as error:
-         print("Error:")
-         print(error)
+      context, query_successful = position_context(request)
+      if query_successful == True:
+         try:
+            if context['cow_id'] == '':
+               cow_id = []
+            else:
+               cow_id = list(map(int, context['cow_id'].split(',')))
+            if context['group_nr'] == '':
+               grp = []
+            else:
+               grp = list(map(int, context['group_nr'].split(',')))
 
-      finally:
+            stats = context['status_list']
+            types = context['position_list']
+            start_date = context['start_date']
+            end_date = context['end_date']
+            start_time = context['start_time']
+            end_time = context['end_time']
+            periodic = context['periodic']
+            #bgPosQuery(cow_id, grp, stats, types,
+                     #start_date, end_date, start_time, end_time, periodic)
+            positionQuery(cow_id, grp, stats, types, start_date, end_date, start_time, end_time, periodic)
+            context['status_message'] = 'Query was successful, file has been generated.'
+         except Exception as error:
+               print('Error: ')
+               print(error)
+               context['status_message'] = 'Failed to query, the following error message were passed: ' + str(error)
+         finally:
+            return render(request, "dblist/dblist_position.html", context)
+      else:
+         context['status_message'] = 'Mandatory input fields are missing, please try again.'
          return render(request, "dblist/dblist_position.html", context)
    
    else:
@@ -76,15 +101,28 @@ def dblist_position(request):
 def dblist_milkdata(request):
 
    if request.method == 'POST':
-      try:
-         print('innan')
-         context = milkdata_context(request)
-         print('efter')
-         return render(request, "dblist/dblist_milkdata.html", context)
-      except Exception as error:
-         print("Error: ")
-         print(error)
-      finally:
+      context, query_successful = milkdata_context(request)
+      if query_successful == True:
+         try:
+            if context['cow_id'] == '':
+               cow_id = []
+            else:
+               cow_id = list(map(int, context['cow_id'].split(',')))
+            if context['group_nr'] == '':
+               grp = []
+            else:
+               grp = list(map(int, context['group_nr'].split(',')))
+            
+            # No function to query
+            context['status_message'] = 'Query was successful, file has been generated.'
+         except Exception as error:
+            print('Error: ')
+            print(error)
+            context['status_message'] = 'Failed to query, the following error message were passed: ' + str(error)
+         finally:
+            return render(request, "dblist/dblist_milkdata.html", context)
+      else:
+         context['status_message'] = 'Mandatory input fields are missing, please try again.'
          return render(request, "dblist/dblist_milkdata.html", context)
    else:
       return render(request, "dblist/dblist_milkdata.html", {})
@@ -93,35 +131,37 @@ def dblist_milkdata(request):
 def dblist_cowinfo(request):
  
    if request.method == 'POST':
-      try:
-         context = cowinfo_context(request)
-         print(context)
-         if context['cow_id'] == '':
-            cow_id = []
-         else:
-            cow_id = list(map(int, context['cow_id'].split(',')))
-         if context['group_nr'] == '':
-            grp = []
-         else:
-            grp = list(map(int, context['group_nr'].split(',')))
-         stats = context['STATUS']
-         start_date = context['start_date']
-         end_date = context['end_date']
-         fields = context['output_list']
-         type = int(context['special_field'])
-         bgInfoQuery(cow_id, grp, stats, start_date, end_date, fields, type)
+      context, query_successful = cowinfo_context(request)
+      if query_successful == True:
+         try:
+            if context['cow_id'] == '':
+               cow_id = []
+            else:
+               cow_id = list(map(int, context['cow_id'].split(',')))
+            if context['group_nr'] == '':
+               grp = []
+            else:
+               grp = list(map(int, context['group_nr'].split(',')))
+            stats = context['STATUS']
+            start_date = context['start_date']
+            end_date = context['end_date']
+            fields = context['output_list']
+            type = int(context['special_field'])
+            #bgInfoQuery(cow_id, grp, stats, start_date, end_date, fields, type)
+            infoQuery(cow_id, grp, stats, start_date, end_date, fields, type)
+            context['status_message'] = 'Query was successful, file has been generated.'
+         except Exception as error:
+            print('Error: ')
+            print(error)
+            context['status_message'] = 'Failed to query, the following error message were passed: ' + str(error)
+         finally:
+            return render(request, "dblist/dblist_cowinfo.html", context)
+      else:
+         context['status_message'] = 'Mandatory input fields are missing, please try again.'
          return render(request, "dblist/dblist_cowinfo.html", context)
-      except Exception as error:
-         print("Error: ")
-         print(error)
 
-      finally:
-         return render(request, "dblist/dblist_cowinfo.html", context)
    else:
       return render(request, "dblist/dblist_cowinfo.html", {})
-
-   return render(request, "dblist/dblist_cowinfo.html", {})
-
 
 
 def dutch_data(request):
--- cow-app/functions.py
+++ cow-app/functions.py
@@ -30,7 +30,12 @@ def milkdata_context(request):
     cow_id = request.POST['cow_id']
     group_nr = request.POST['group_nr']
     start_date = request.POST['start_date']
-    end_date = request.POST['end_date']     
+    end_date = request.POST['end_date']
+
+    query_successful = True
+    fetch_message = ''
+    user_inputs = []
+    
     
     if "all_types" in request.POST:
         status_list = [
@@ -63,20 +68,38 @@ def milkdata_context(request):
 
     if "TIDIG" in request.POST:
         status_list.append('TIDIG')
-   
+    
+
+    # User feedback when "Fetch data" is pressed. Check for invalid inputs.
+    if status_list == [] or start_date == '' or end_date == '':
+        query_successful = False
+        fetch_message = 'User input missing: '
+        if status_list == []:
+            user_inputs.append('status type')
+        if start_date == '':
+            user_inputs.append('start date')
+        if end_date == '':
+            user_inputs.append('end date')
+    else:
+        user_inputs = ['Cow id: ' + cow_id, 'Group number: ' + group_nr,",".join(['Status types selected: '] + status_list),'Start date: ' + start_date, 'End date: ' + end_date]
+        fetch_message = 'User input:'
+
     context = {
         'cow_id': cow_id,
         'group_nr': group_nr,
         'start_date': start_date,
         'end_date': end_date,
         'status_list': status_list,
+        'fetch_message': fetch_message,
+        'user_inputs': user_inputs,
     }
     
-    return context
+    return context, query_successful
 
 
 
 def position_context(request):
+
     status_list = []
     position_list = []
     cow_id = request.POST['cow_id']
@@ -85,6 +108,10 @@ def position_context(request):
     end_time = request.POST['end_time']
     start_date = request.POST['start_date']
     end_date = request.POST['end_date']
+
+    fetch_message = ''
+    user_inputs = []
+    query_successful = True
 
 
     if "Periodic" in request.POST:
@@ -105,7 +132,6 @@ def position_context(request):
     if "PC" in request.POST:
         position_list.append('PC')
 
-
     
     if "all_types" in request.POST:
         status_list = [
@@ -139,6 +165,30 @@ def position_context(request):
     if "TIDIG" in request.POST:
         status_list.append('TIDIG')
 
+        # User feedback when "Fetch data" is pressed. Check for invalid inputs.
+    if status_list == [] or start_date == '' or end_date == '' or start_time=='' or end_time == '' or position_list == []:
+        query_successful = False
+        fetch_message = 'User input missing: '
+        if status_list == []:
+            user_inputs.append('status type')
+        if position_list == []:
+            user_inputs.append('position data')
+        if start_date == '':
+            user_inputs.append('start date'),
+        if end_date == '':
+            user_inputs.append('end date')
+        if start_time == '':
+            user_inputs.append('start time')
+        if end_time == '':
+            user_inputs.append('end time')
+    else:
+        user_inputs = ['Cow id: ' + cow_id, 'Group number: ' + group_nr,",".join(['Status types selected: '] + status_list), ['Position type(s): '] + position_list,'Start date: ' + start_date, 'End date: ' + end_date,
+        'Start time: ' + start_time, 'End time: ' + end_time, 'Periodic: ' + str(periodic)]
+        fetch_message = 'User inputs:'
+        
+
+    
+
     context = {
         'cow_id': cow_id,
         'group_nr': group_nr,
@@ -148,20 +198,28 @@ def position_context(request):
         'end_date': end_date,
         'periodic': periodic,
         'position_list': position_list,
-        'status_list': status_list
+        'status_list': status_list,
+        'fetch_message': fetch_message,
+        'user_inputs': user_inputs,
     }
-    return context
+    return context, query_successful
 
 
 def cowinfo_context(request):
+  
     STATUS = []
     output_list = []
     special_field = request.POST['special_field']
     cow_id = request.POST['cow_id']
     group_nr = request.POST['group_nr']
     start_date = request.POST['start_date']
-    end_date = request.POST['end_date']     
-    
+    end_date = request.POST['end_date']
+    requested_list = []
+
+    user_inputs = []
+    fetch_message = ''
+    query_successful = True
+
 
     # -- STATUS LIST PARAMETERS -- #      
     if "all_types" in request.POST:
@@ -196,6 +254,7 @@ def cowinfo_context(request):
     if "TIDIG" in request.POST:
         STATUS.append('TIDIG')
 
+
     # ---------------------- #
 
     # ---- Requested Output Parameters -------- #
@@ -203,28 +262,59 @@ def cowinfo_context(request):
     # order: "resp", "grp", "stat", "lakt", "kalvn_date"
     if "resp_nr" in request.POST:
         output_list.append(True)
+        requested_list.append('resp_nr')
     else:
         output_list.append(False)
 
     if "Group" in request.POST:
         output_list.append(True)
+        requested_list.append('Group')
     else:
         output_list.append(False)
 
     if "status" in request.POST:
         output_list.append(True)
+        requested_list.append('status')
     else:
         output_list.append(False)
 
     if "lactation_nr" in request.POST:
         output_list.append(True)
+        requested_list.append('lactation number')
     else:
         output_list.append(False)
     
     if "kalvn_date" in request.POST:
         output_list.append(True)
+        requested_list.append('kalvn date')
     else:
         output_list.append(False)
+
+    
+    if special_field == '0':
+        special_field_feedback = 'General information'
+    elif special_field == '1':
+        special_field_feedback = 'Health data'
+    else:
+        special_field_feedback = 'Insemination data'
+
+    # User feedback when "Fetch data" is pressed. Check for invalid inputs.
+    if STATUS == [] or start_date == '' or end_date == '' or not any(output_list):
+        query_successful = False
+        fetch_message = 'User input missing: '
+        if STATUS == []:
+            user_inputs.append('status type')
+        if start_date == '':
+            user_inputs.append('start date')
+        if end_date == '':
+            user_inputs.append('end date')
+        if not any(output_list):
+            user_inputs.append('requested output')
+    else:
+        user_inputs = ['Cow id: ' + cow_id, 'Group number: ' + group_nr,", ".join(['Status types selected: '] + STATUS), 'Start date: ' + start_date, 'End date: ' + end_date,
+        ", ".join(['Requested outputs: '] + requested_list), 'Special field: ' + special_field_feedback]
+        fetch_message = 'User input:'
+    
     
 
     context = {
@@ -235,5 +325,7 @@ def cowinfo_context(request):
         'STATUS': STATUS,
         'output_list': output_list,
         'special_field': special_field,
+        'fetch_message': fetch_message,
+        'user_inputs': user_inputs,
     }
-    return context
+    return context, query_successful
