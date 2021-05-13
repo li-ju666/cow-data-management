a18a0bd6f3f9088c85c9ecf6dc830bb9aacb6f6f
Li Ju <li.ju.0122@student.uu.se>
2021-05-13 18:31:36 +0200

    usage scripts

--- run.sh
+++ run.sh
@@ -7,6 +7,4 @@ mkdir -p upload_files/nl
 sudo chmod 777 -R upload_files
 sudo chmod 777 -R result_files
 sudo chmod 777 -R log
-# sudo rm mysql -rf
-# sudo rm result_files/* -f
-docker-compose up --build
+docker-compose up --build -d
